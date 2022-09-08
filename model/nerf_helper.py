28e0a7ba0c0144d4e66cca71eca358c189ab270c
설재중 <tjfwownd@naver.com>
2022-09-08 10:02:40 +0900

    9.8

--- model/nerf_helper.py
+++ model/nerf_helper.py
@@ -76,6 +76,54 @@ def transform_sampled_points(points, z_vals, ray_directions, device, h_stddev=1,
 
     return transformed_points[..., :3], z_vals, transformed_ray_directions, transformed_ray_origins, pitch, yaw
 
+def sample_camera_positions(device, n=1, r=1, horizontal_stddev=1, vertical_stddev=1, horizontal_mean=math.pi*0.5, vertical_mean=math.pi*0.5, mode='normal'):
+    """
+    Samples n random locations along a sphere of radius r. Uses the specified distribution.
+    Theta is yaw in radians (-pi, pi)
+    Phi is pitch in radians (0, pi)
+    """
+
+    if mode == 'uniform':
+        theta = (torch.rand((n, 1), device=device) - 0.5) * 2 * horizontal_stddev + horizontal_mean
+        phi = (torch.rand((n, 1), device=device) - 0.5) * 2 * vertical_stddev + vertical_mean
+
+    elif mode == 'normal' or mode == 'gaussian':
+        theta = torch.randn((n, 1), device=device) * horizontal_stddev + horizontal_mean
+        phi = torch.randn((n, 1), device=device) * vertical_stddev + vertical_mean
+
+    elif mode == 'hybrid':
+        if random.random() < 0.5:
+            theta = (torch.rand((n, 1), device=device) - 0.5) * 2 * horizontal_stddev * 2 + horizontal_mean
+            phi = (torch.rand((n, 1), device=device) - 0.5) * 2 * vertical_stddev * 2 + vertical_mean
+        else:
+            theta = torch.randn((n, 1), device=device) * horizontal_stddev + horizontal_mean
+            phi = torch.randn((n, 1), device=device) * vertical_stddev + vertical_mean
+
+    elif mode == 'truncated_gaussian':
+        theta = truncated_normal_(torch.zeros((n, 1), device=device)) * horizontal_stddev + horizontal_mean
+        phi = truncated_normal_(torch.zeros((n, 1), device=device)) * vertical_stddev + vertical_mean
+
+    elif mode == 'spherical_uniform':
+        theta = (torch.rand((n, 1), device=device) - .5) * 2 * horizontal_stddev + horizontal_mean
+        v_stddev, v_mean = vertical_stddev / math.pi, vertical_mean / math.pi
+        v = ((torch.rand((n,1), device=device) - .5) * 2 * v_stddev + v_mean)
+        v = torch.clamp(v, 1e-5, 1 - 1e-5)
+        phi = torch.arccos(1 - 2 * v)
+
+    else:
+        # Just use the mean.
+        theta = torch.ones((n, 1), device=device, dtype=torch.float) * horizontal_mean
+        phi = torch.ones((n, 1), device=device, dtype=torch.float) * vertical_mean
+
+    phi = torch.clamp(phi, 1e-5, math.pi - 1e-5)
+
+    output_points = torch.zeros((n, 3), device=device)
+    output_points[:, 0:1] = r*torch.sin(phi) * torch.cos(theta)
+    output_points[:, 2:3] = r*torch.sin(phi) * torch.sin(theta)
+    output_points[:, 1:2] = r*torch.cos(phi)
+
+    return output_points, phi, theta
+
 def truncated_normal_(tensor, mean=0, std=1):
     """ tensor trunction 진행
 
@@ -141,4 +189,5 @@ def perturb_points(points, z_vals, ray_directions, device):
 
     points = points + offset * ray_directions.unsqueeze(2)
     return points, z_vals
+
 
